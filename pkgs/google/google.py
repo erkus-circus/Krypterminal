8b584a80e6b90c9c8b34cf9d42bf33984c7236a3
erkus-circus <43392660+erkus-circus@users.noreply.github.com>
2018-11-07 16:47:33 -0500

    Got rid of pkgs

--- pkgs/google/google.py
+++
@@ -1,5 +0,0 @@
-import webbrowser as wbb
-words = command.split()
-if len(words) < 2:
-    words = [1,'']
-wbb.get().open_new_tab('https://www.google.com/search?q=' + '%20'.join(words[1:]))
--- pkgs/web/web.py
+++
@@ -1,6 +0,0 @@
-import webbrowser as wbb
-
-if len(command.split()) < 2:
-    print('Usage: web <URL>')
-else:
-    wbb.get().open_new_tab(command.split()[1])
